743496a2e65d2ab5f969e0a12e0e86ad205ba49e
Lagz0ne <duc@outlook.sg>
2023-08-06 11:11:51 +0700

    feat: getting started

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Native World!");
+    }
+
+    public String echo(String input) {
+        return "Hello from " + input;
+    }
+
+}
